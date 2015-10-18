d7cf1f6ffc6a7f2baeb61337c7b5304b9af88042
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-10-18 17:03:07 +0200

    pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,13 +5,14 @@ idea: crear una lista con etiquetas y diccionarios
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-    
+
         self.lista = []
         self.root = {}
-        self.reg ={}
+        self.reg = {}
         self.img = {}
         self.aud = {}
         self.text = {}
@@ -20,42 +21,39 @@ class SmallSMILHandler(ContentHandler):
 
         if etiqueta == 'root-layout':
             self.lista.append("root-layout")
-            self.root["width"] = attrs.get('width',"")
-            self.root["height"] = attrs.get('height',"")
-            self.root["background"] = attrs.get('background-color',"")
+            self.root["width"] = attrs.get('width', "")
+            self.root["height"] = attrs.get('height', "")
+            self.root["background"] = attrs.get('background-color', "")
             self.lista.append(self.root)
         elif etiqueta == 'region':
             self.lista.append("region")
-            self.reg["id"] = attrs.get('id',"")
-            self.reg["top"] = attrs.get('top',"")
-            self.reg["bottom"] = attrs.get('bottom',"")
-            self.reg["left"] = attrs.get('left',"")
-            self.reg["right"] = attrs.get('right',"")
+            self.reg["id"] = attrs.get('id', "")
+            self.reg["top"] = attrs.get('top', "")
+            self.reg["bottom"] = attrs.get('bottom', "")
+            self.reg["left"] = attrs.get('left', "")
+            self.reg["right"] = attrs.get('right', "")
             self.lista.append(self.reg)
         elif etiqueta == 'img':
             self.lista.append("img")
-            self.img["src"] = attrs.get('src',"")
-            self.img["region"] = attrs.get('region',"")
-            self.img["begin"] = attrs.get('begin',"")
-            self.img["dur"] = attrs.get('dur',"")
+            self.img["src"] = attrs.get('src', "")
+            self.img["region"] = attrs.get('region', "")
+            self.img["begin"] = attrs.get('begin', "")
+            self.img["dur"] = attrs.get('dur', "")
             self.lista.append(self.img)
         elif etiqueta == 'audio':
             self.lista.append("audio")
-            self.aud["src"] = attrs.get('src',"")
-            self.aud["begin"] = attrs.get('begin',"")
-            self.aud["dur"] = attrs.get('dur',"")
+            self.aud["src"] = attrs.get('src', "")
+            self.aud["begin"] = attrs.get('begin', "")
+            self.aud["dur"] = attrs.get('dur', "")
             self.lista.append(self.aud)
         elif etiqueta == 'textstream':
             self.lista.append("textsream")
-            self.text["src"] = attrs.get('src',"")
-            self.text["region"] = attrs.get('region',"")
+            self.text["src"] = attrs.get('src', "")
+            self.text["region"] = attrs.get('region', "")
             self.lista.append(self.text)
-    
+
     def get_tags(self):
         return self.lista
-        
-       
-
 if __name__ == "__main__":
 
     parser = make_parser()
